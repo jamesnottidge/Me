ebfeec04dc1c20bca2584b5d10ff520eda608e67
jamesnottidge <freshice57@gmail.com>
2023-10-20 22:04:50 +0100

    project schemas definition

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
-import {deskTool} from "sanity/desk";
+import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "ngwkfhf6",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-10-20",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: schemas },
 });
 
 export default config;
