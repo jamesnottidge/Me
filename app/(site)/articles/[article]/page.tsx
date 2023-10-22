917a4c76fe7dcb849df44095fcbbc3488f0beeb6
jamesnottidge <freshice57@gmail.com>
2023-10-22 07:30:31 +0100

    article page

--- app/(site)/articles/[article]/page.tsx
+++ app/(site)/articles/[article]/page.tsx
@@ -1,45 +1,36 @@
-import { getProject } from "@/sanity/sanity-utils";
+import { getArticle } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
-import Image from "next/image";
 
 type Props = {
-  params: { project: string };
+  params: { article: string };
+};
+
+const components = {
+  block: {
+    // Add a custom component for rendering paragraphs with spacing
+    normal: ({ children }) => (
+      <p className="my-20">{children}</p> // You can adjust the margin (my-4) as needed
+    ),
+    // Define other block types as needed
+  },
 };
 
 export default async function Project({ params }: Props) {
-  const slug = params.project;
-  const project = await getProject(slug);
+  const slug = params.article;
+  const article = await getArticle(slug);
   return (
     <div>
       <header className="flex justify-between items-center">
-        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 text-5xl drop-shadow font-extrabold">
-          {project.name}
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 lg:text-5xl text-4xl drop-shadow font-extrabold">
+          {article.title}
         </h1>
-        <a
-          href={project.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          title="View Project"
-          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
-        >
-          View Project
-        </a>
       </header>
-
       {/* content  */}
 
-      <div className="text-lg text-gray-700 mt-5">
-        <PortableText value={project.content} />
+      <div className="text-xl lg:text-3xl mt-8">
+             <PortableText value={article.content} components={components} />
+        
       </div>
-
-      {/* image  */}
-      <Image
-        src={project.image}
-        alt={project.name}
-        width={1920}
-        height={1080}
-        className="mt-10 border-2 border-gray-500 object-cover rounded-xl"
-      />
     </div>
   );
 }
